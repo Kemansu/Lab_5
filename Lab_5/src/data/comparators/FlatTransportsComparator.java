4c9a72a1df6ad0db4b0107244afa2f6c6e378db5
Kemansu <153510756+Kemansu@users.noreply.github.com>
2024-03-22 13:04:25 +0300

    Update FlatTransportsComparator.java

--- Lab_5/src/data/comparators/FlatTransportsComparator.java
+++ Lab_5/src/data/comparators/FlatTransportsComparator.java
@@ -9,36 +9,8 @@ public class FlatTransportsComparator implements Comparator<Flat> {
 
     @Override
     public int compare(Flat flat1, Flat flat2) {
-        Integer f1 = null;
-        Integer f2 = null;
 
-
-        if (flat1.getTransport() == Transport.NONE){
-            f1 = 1;
-        } else if (flat1.getTransport() == Transport.FEW) {
-            f1 = 2;
-        } else if (flat1.getTransport() == Transport.LITTLE) {
-            f1 = 3;
-        } else if (flat1.getTransport() == Transport.NORMAL) {
-            f1 = 4;
-        } else if (flat1.getTransport() == Transport.ENOUGH) {
-            f1 = 5;
-        }
-
-        if (flat2.getTransport() == Transport.NONE){
-            f2 = 1;
-        } else if (flat2.getTransport() == Transport.FEW) {
-            f2 = 2;
-        } else if (flat2.getTransport() == Transport.LITTLE) {
-            f2 = 3;
-        } else if (flat2.getTransport() == Transport.NORMAL) {
-            f2 = 4;
-        } else if (flat2.getTransport() == Transport.ENOUGH) {
-            f2 = 5;
-        }
-
-
-        return Integer.compare(f1, f2);
+        return flat1.getTransport().compareTo(flat2.getTransport());
 
     }
 }
