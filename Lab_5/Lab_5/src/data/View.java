4d44dae66b93073d5e81df97ea1da9c2b503f9b8
Kemansu <153510756+Kemansu@users.noreply.github.com>
2024-03-22 12:59:07 +0300

    Delete Lab_5/Lab_5 directory

--- Lab_5/Lab_5/src/data/View.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public enum View {
-    PARK,
-    BAD,
-    NORMAL;
-}
--- Lab_5/Lab_5/src/manager/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package manager.commands;
-
-public interface Command {
-    public void execute(String[] args) throws Exception;
-
-    public String getName();
-
-    public String getDescription();
-}
