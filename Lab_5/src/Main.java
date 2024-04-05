ee21ef723c63a81df3ba8fc779a27807c2d26fd8
deniapogosov <pogosovdenis@gmail.com>
2024-04-05 12:24:45 +0300

    правки_1

--- Lab_5/src/Main.java
+++ Lab_5/src/Main.java
@@ -5,7 +5,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Данный класс запускает работу консольного приложения
+ *
+ * @author Kemansu
+ * @since 1.0
+ */
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         Console console = new Console();
